efd3f8f692e646195f66249fb4d687cfa6024fda
northernman86 <aliaksandrprystauka@gmail.com>
2022-04-17 22:23:51 +0300

    Add project files.

---
+++ sendingEmailSMTP.py
@@ -0,0 +1,23 @@
+import smtplib
+
+domainName = input('Enter SMTP server domain name\n')
+port = int(input('Enter port\n'))
+
+if (port == 587):
+    smtpObject = smtplib.SMTP(domainName, port)
+    smtpObject.ehlo()
+    smtpObject.starttls()
+if (port == 465):
+    smtpObject = smtplib.SMTP_SSL(domainName, port)
+    smtpObject.ehlo()
+
+emailOut = input('Enter your email address\n')
+password = input('Enter password\n')
+smtpObject.login(emailOut, password)
+
+emailIn = input('Enter the destination email address\n')
+subject = input('Enter subject\n')
+text = input('Enter text\n')
+message = 'Subject: ' + subject + '\n ' + text
+smtpObject.sendmail(emailOut, emailIn, message)
+smtpObject.quit()
